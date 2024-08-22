72fe1e73005fe69bcc4602662b0c61ce5a1615c9
Sam Cantrell <1220819+samcan@users.noreply.github.com>
2024-08-22 18:42:36 -0500

    Add point and vector tuples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
 @main def hello(): Unit =
   println("Hello world!")
-  println(msg)
 
-def msg = "I was compiled by Scala 3. :)"
+case class Tuple(x: Double, y: Double, z: Double, w: Double)
+
+def tuple_is_a_point(t: Tuple): Boolean = {
+  t.w == 1.0
+}
+
+def tuple_is_a_vector(t: Tuple): Boolean = {
+  !tuple_is_a_point(t)
+}
+
+def point(x: Double, y: Double, z: Double): Tuple = {
+  Tuple(x, y, z, 1.0)
+}
+
+def vector(x: Double, y: Double, z: Double): Tuple = {
+  Tuple(x, y, z, 0.0)
+}
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,25 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+  test("A tuple with w=1.0 is a point") {
+    val input = Tuple(4.3, -4.2, 3.1, 1.0)
+    assertEquals(tuple_is_a_point(input), true)
+    assertEquals(tuple_is_a_vector(input), false)
+  }
+
+  test("A tuple with w=0.0 is a vector") {
+    val input = Tuple(4.3, -4.2, 3.1, 0.0)
+    assertEquals(tuple_is_a_vector(input), true)
+    assertEquals(tuple_is_a_point(input), false)
+  }
+
+  test("point() creates a tuple with w=1.0") {
+    val output = point(4, -4, 3)
+    assertEquals(tuple_is_a_point(output), true)
+  }
+
+  test("vector() creates a tuple with w=0.0") {
+    val output = vector(4, -4, 3)
+    assertEquals(tuple_is_a_vector(output), true)
   }
 }
